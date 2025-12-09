c24477d2e8d094d9c5a36c695f63b645b2084648
yuka-matsuki <yuka_nagata@mail.toyota.co.jp>
2025-12-09 15:53:38 +0900

    DB接続

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.postgresql:postgresql:42.7.2")
 }
 
 kotlin {
